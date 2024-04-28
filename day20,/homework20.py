8a1ee1ad55101e3966da2d5fb5da8359f8824ebe
matenikoleihsvili060 <matenikoleishvili060@gmail.com>
2024-04-28 09:01:50 +0400

    commit

--- day20,/homework20.py
+++ day20,/homework20.py
@@ -1,1 +1,54 @@
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
 
+def number_to_string(num):
+    return str(num)
+
+
+def opposite(number):
+  return number * (-1)
+
+
+def make_negative( number ):
+    if number > 0:
+        return number * (-1)
+    else:
+        return number
+    
+
+def bool_to_word(boolean):
+    if boolean:
+        return "Yes"
+    else:
+        return "No"
+    
+
+def positive_sum(numbers_list):
+    sum = 0
+    
+    for number in numbers_list:
+        if number > 0:
+            sum = sum + number
+    
+    return sum
+
+
+def remove_char(s):
+    return s[1:-1]
+
+
+def square_sum(numbers):
+    sum = 0
+    
+    for num in numbers:
+        sum = sum + (num * num)
+    
+    return sum
+
+
+def double_integer(i):
+    return i * 2
