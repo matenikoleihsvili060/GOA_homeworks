30795fb846c04b9afc5f0928480102c2e3a42499
matenikoleihsvili060 <matenikoleishvili060@gmail.com>
2024-05-16 19:36:07 +0400

    commit

---
+++ day26,/claswork26.py/lesson26,1.py
@@ -0,0 +1,14 @@
+def print_name(name,amount = 5):
+    for i in range(amount):
+        print(name)
+
+print_name("luka",10)
+
+
+def print_name(name,amount = 5):
+    names = []
+    for i in range(amount):
+        names.append(name)
+        
+
+print_name("luka",10)
